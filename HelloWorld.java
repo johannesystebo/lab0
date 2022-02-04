cfe1284303d8470b9bc6bbcb08713ea689cafa56
johannesystebo <98183418+johannesystebo@users.noreply.github.com>
2022-02-04 09:09:39 +0100

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,29 +1,5 @@
-package lab0;
-
-/**
- * Skriver ut Hello world!
- *
- * @author Anna Eilertsen - anna.eilertsen@uib.no
- *
- */
-public class HelloWorld {
-	
-	/*
-	 * TODO: Set this variable to true
-	 */
-	public final static boolean FINISHED_LAB0 = true;
-
-	public static void main(String[] args) {
-		String helloWorld = getHelloWorld();
-		System.out.println(helloWorld);
-	}
-
-	/**
-	 * Henter en Hello World-{@link String}
-	 *
-	 * @return "Hello World!"
-	 */
-	public static String getHelloWorld() {
-		return "Hello World!";
-	}
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); 
+    }
 }
